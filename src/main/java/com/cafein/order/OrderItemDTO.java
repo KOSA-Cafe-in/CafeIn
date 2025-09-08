c5bb89ab67e5a6aadf0218758dec111a0dabb4b1
Natest <62326659+ncb6206@users.noreply.github.com>
2025-09-08 10:14:00 +0900

    어드민 페이지 구현 (#7)
    * Refactor : views 폴더 구조 추가
    
    * Refactor : views 폴더 구조 추가 (#3)
    
    Co-authored-by: ycson <ymj06222@naver.com>
    
    * Delete : 기존 폴더 구조 삭제
    
    * Refactor : 폴더 구조 변경
    
    * feat : 기능구현
    
    ---------
    
    Co-authored-by: ycson <ymj06222@naver.com>
    Co-authored-by: ycson <104715028+syc2618@users.noreply.github.com>

--- src/main/java/com/cafein/order/OrderItemDTO.java
+++ src/main/java/com/cafein/order/OrderItemDTO.java
@@ -9,4 +9,10 @@ public class OrderItemDTO {
     private Long menuId;
     private Long count;
     private Long unitPrice;
+
+    // ✅ 메뉴 이름도 함께 가져오면 JSP에서 더 편리
+    private String menuName;
+
+    // 👇 추가 추천
+    private String menuPictureUrl; // 메뉴 썸네일 같이 보여주려면
 }
